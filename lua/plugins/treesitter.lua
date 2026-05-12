8d7731ccecc22975657b06a137ce3c4f1cf7056e
Matt Fair <mfair@zylo.com>
2026-05-12 11:07:20 -0400

    update treesitter

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,16 +1,21 @@
 return {
   "nvim-treesitter/nvim-treesitter",
-  branch = "master",
+  branch = "main",
+  lazy = false,
   build = ":TSUpdate",
   config = function()
-    require("nvim-treesitter.configs").setup({
-      ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "javascript", "typescript", "python" },
-      sync_install = false,
-      auto_install = true,
-      highlight = {
-        enable = true,
-        additional_vim_regex_highlighting = false,
+    require("nvim-treesitter").install({
+      "c", "lua", "vim", "vimdoc", "query",
+      "javascript", "typescript", "tsx", "python",
+    })
+    vim.api.nvim_create_autocmd("FileType", {
+      pattern = {
+        "c", "lua", "vim", "help", "query",
+        "javascript", "typescript", "typescriptreact", "python",
       },
+      callback = function()
+        pcall(vim.treesitter.start)
+      end,
     })
   end,
 }
